5ab4863dfaa2ce84aff5f652ae9ccf2e6cb3f343
Jia Chang <smile001108@163.com>
2020-11-11 20:26:34 +0800

    添加跨域请求的解决方法

---
+++ src/main/java/com/jiac/restaurantsystem/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.jiac.restaurantsystem.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * FileName: WebConfig
+ * Author: Jiac
+ * Date: 2020/11/11 20:24
+ */
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
